839f52b492db6b1ce9d1b10934398dde318aca9d
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-06-10 10:02:18 +0100

    Adding s3 bucket

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -24,6 +24,11 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
+resource "aws_s3_bucket" "terraform-state-housing-development" {
+  bucket = "terraform-state-housing-search-listener"
+  acl    = "private"
+}
+
 terraform {
   backend "s3" {
     bucket  = "terraform-state-housing-search-listener"
