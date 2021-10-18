0bdca1e97ca1a0a75baf5c3a4a5aab5ca6bc7143
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-10-18 14:55:17 +0100

    Updated account tenure type

--- HousingSearchListener/V1/Domain/Account/AccountTenure.cs
+++ HousingSearchListener/V1/Domain/Account/AccountTenure.cs
@@ -5,7 +5,7 @@ namespace HousingSearchListener.V1.Domain.Account
     public class AccountTenure
     {
         public string TenureId { get; set; }
-        public string TenureType { get; set; }
+        public AccountTenureType TenureType { get; set; }
         public List<AccountTenant> PrimaryTenants { get; set; }
         public string FullAddress { get; set; }
     }
