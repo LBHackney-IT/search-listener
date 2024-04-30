6a90eb3a02ddbc30bfac8794b4ddee14dd85584c
LBHTKarki <tuomo.karki@hackney.gov.uk>
2024-04-30 11:49:03 +0100

    format

--- HousingSearchListener/V1/Domain/Tenure/TenureInformation.cs
+++ HousingSearchListener/V1/Domain/Tenure/TenureInformation.cs
@@ -13,7 +13,7 @@ namespace HousingSearchListener.V1.Domain.Tenure
         public bool IsActive { get; set; }
         public List<HouseholdMembers> HouseholdMembers { get; set; }
         public string PaymentReference { get; set; }
-        
+
         //use TempAccommodationInfo from Hackney.Shared.HousingSearch.Domain.Tenure to avoid duplicating objects
         public TempAccommodationInfo TempAccommodationInfo { get; set; }
     }
